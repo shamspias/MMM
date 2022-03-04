194bd90d13ca2b422724251e90d38849e559bc00
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2022-03-04 22:01:25 +0600

    view added for expanse and loan

--- money_management/views.py
+++ money_management/views.py
@@ -6,3 +6,89 @@ from django.urls import reverse_lazy
 
 from .models import Expenses, Loans, MoneyManagement
 from .forms import ExpensesForm, LoansForm, MoneyManagementForm
+
+
+class ExpensesListView(ListView):
+    """
+    Show List of all Expanses
+    """
+    model = Expenses
+
+
+class ExpensesDetailView(DetailView):
+    """
+    Show A Single Expanses
+    """
+    model = Expenses
+
+
+class ExpensesCreate(SuccessMessageMixin, CreateView):
+    """
+    Create an Expanse
+    """
+    model = Expenses
+    form_class = ExpensesForm
+    success_url = reverse_lazy('expenses_list')
+    success_message = "Expense Added!"
+
+
+class ExpensesUpdate(SuccessMessageMixin, UpdateView):
+    """
+    Update an Expense
+    """
+    model = Expenses
+    form_class = ExpensesForm
+    success_url = reverse_lazy('expenses_list')
+    success_message = "Expense updated!"
+
+
+class ExpensesDelete(SuccessMessageMixin, DeleteView):
+    """
+    Delete an Expanse
+    """
+    model = Expenses
+    success_url = reverse_lazy('expenses_list')
+    success_message = "Expense deleted!"
+
+
+class LoansListView(ListView):
+    """
+    Show List of all Loans
+    """
+    model = Loans
+
+
+class LoansDetailView(DetailView):
+    """
+    Show A Single Loans
+    """
+    model = Loans
+
+
+class LoansCreate(SuccessMessageMixin, CreateView):
+    """
+    Create a Loan
+    """
+    model = Loans
+    form_class = LoansForm
+    success_url = reverse_lazy('loans_list')
+    success_message = "Loan Added!"
+
+
+class LoansUpdate(SuccessMessageMixin, UpdateView):
+    """
+    Update a Loan
+    """
+    model = Loans
+    form_class = LoansForm
+    success_url = reverse_lazy('loans_list')
+    success_message = "Loan updated!"
+
+
+class LoansDelete(SuccessMessageMixin, DeleteView):
+    """
+    Delete a Loan
+    """
+    model = Loans
+    success_url = reverse_lazy('loans_list')
+    success_message = "Loan deleted!"
