6c49599fd800864518e7519183db6678b6c238c1
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2022-03-04 23:13:09 +0600

    add month field

--- money_management/models.py
+++ money_management/models.py
@@ -21,11 +21,26 @@ class Loans(models.Model):
 
 
 class MoneyManagement(models.Model):
+    CHOICE_MONTH = (
+        ('January', 'Jan'),
+        ('February', 'Feb'),
+        ('March', 'March'),
+        ('April', 'Apr'),
+        ('May', 'May'),
+        ('June', 'Jun'),
+        ('July', 'Jul'),
+        ('August', 'Aug'),
+        ('September', 'Sep'),
+        ('October', 'Oct'),
+        ('November', 'Nov'),
+        ('December', 'Dec'),
+
+    )
     salary = models.DecimalField(default=0.0, decimal_places=2, max_digits=10)
     expense = models.ForeignKey(Expenses, on_delete=models.CASCADE, blank=True, null=True)
     loan = models.ForeignKey(Loans, on_delete=models.CASCADE, blank=True, null=True)
     year = models.CharField(max_length=10, blank=True, null=True)
-    month = models.CharField(max_length=50, blank=True, null=True)
+    month = models.CharField(max_length=50, choices=CHOICE_MONTH, blank=True, null=True)
     date = models.DateField(auto_now=True)
 
     def __str__(self):
