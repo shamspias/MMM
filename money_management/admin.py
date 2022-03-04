2311f00b2dabd6d73198d8dcd3ec2fd8e26b5bad
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2022-03-04 21:49:40 +0600

    Add to admin

--- money_management/admin.py
+++ money_management/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Expenses, Loans, MoneyManagement
+
 # Register your models here.
+
+
+admin.site.register(Expenses)
+admin.site.register(Loans)
+admin.site.register(MoneyManagement)
