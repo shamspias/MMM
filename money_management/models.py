5d62ebac598158c915fa9a78265bc5328e274b31
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2022-03-04 21:48:25 +0600

    model added

--- money_management/models.py
+++ money_management/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Expenses(models.Model):
     name = models.CharField(max_length=250, blank=True, null=True)
     amount = models.DecimalField(default=0.0)
