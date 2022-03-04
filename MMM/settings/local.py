f7b5bf2d943369ffb82796300878463fc7e91d84
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2022-03-04 21:29:21 +0600

    update setting

---
+++ MMM/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
+
+DATABASES = {
+    'default': {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USERNAME'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
