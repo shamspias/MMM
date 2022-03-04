cb11a90b59a157fdfef440b89c28bf7e5407cae0
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2022-03-04 22:14:08 +0600

    update view with Money Management

--- money_management/views.py
+++ money_management/views.py
@@ -3,26 +3,26 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Expenses, Loans, MoneyManagement
 from .forms import ExpensesForm, LoansForm, MoneyManagementForm
 
 
-class ExpensesListView(ListView):
+class ExpensesListView(ListView, LoginRequiredMixin):
     """
     Show List of all Expanses
     """
     model = Expenses
 
 
-class ExpensesDetailView(DetailView):
+class ExpensesDetailView(DetailView, LoginRequiredMixin):
     """
     Show A Single Expanses
     """
     model = Expenses
 
 
-class ExpensesCreate(SuccessMessageMixin, CreateView):
+class ExpensesCreate(SuccessMessageMixin, CreateView, LoginRequiredMixin):
     """
     Create an Expanse
     """
@@ -32,7 +32,7 @@ class ExpensesCreate(SuccessMessageMixin, CreateView):
     success_message = "Expense Added!"
 
 
-class ExpensesUpdate(SuccessMessageMixin, UpdateView):
+class ExpensesUpdate(SuccessMessageMixin, UpdateView, LoginRequiredMixin):
     """
     Update an Expense
     """
@@ -42,7 +42,7 @@ class ExpensesUpdate(SuccessMessageMixin, UpdateView):
     success_message = "Expense updated!"
 
 
-class ExpensesDelete(SuccessMessageMixin, DeleteView):
+class ExpensesDelete(SuccessMessageMixin, DeleteView, LoginRequiredMixin):
     """
     Delete an Expanse
     """
@@ -51,21 +51,21 @@ class ExpensesDelete(SuccessMessageMixin, DeleteView):
     success_message = "Expense deleted!"
 
 
-class LoansListView(ListView):
+class LoansListView(ListView, LoginRequiredMixin):
     """
     Show List of all Loans
     """
     model = Loans
 
 
-class LoansDetailView(DetailView):
+class LoansDetailView(DetailView, LoginRequiredMixin):
     """
     Show A Single Loans
     """
     model = Loans
 
 
-class LoansCreate(SuccessMessageMixin, CreateView):
+class LoansCreate(SuccessMessageMixin, CreateView, LoginRequiredMixin):
     """
     Create a Loan
     """
@@ -75,7 +75,7 @@ class LoansCreate(SuccessMessageMixin, CreateView):
     success_message = "Loan Added!"
 
 
-class LoansUpdate(SuccessMessageMixin, UpdateView):
+class LoansUpdate(SuccessMessageMixin, UpdateView, LoginRequiredMixin):
     """
     Update a Loan
     """
@@ -85,10 +85,53 @@ class LoansUpdate(SuccessMessageMixin, UpdateView):
     success_message = "Loan updated!"
 
 
-class LoansDelete(SuccessMessageMixin, DeleteView):
+class LoansDelete(SuccessMessageMixin, DeleteView, LoginRequiredMixin):
     """
     Delete a Loan
     """
     model = Loans
     success_url = reverse_lazy('loans_list')
     success_message = "Loan deleted!"
+
+
+class MoneyManagementListView(ListView, LoginRequiredMixin):
+    """
+    Show List of Money Management list
+    """
+    model = MoneyManagement
+
+
+class MoneyManagementDetailsView(ListView, LoginRequiredMixin):
+    """
+    Show List of Money Management Details
+    """
+    model = MoneyManagement
+
+
+class MoneyManagementCreateView(SuccessMessageMixin, CreateView, LoginRequiredMixin):
+    """
+    Create Money Management
+    """
+    model = MoneyManagement
+    form_class = MoneyManagementForm
+    success_url = reverse_lazy('mm_list')
+    success_message = "MM Added!"
+
+
+class MoneyManagementUpdateView(SuccessMessageMixin, UpdateView, LoginRequiredMixin):
+    """
+    Update Money Management
+    """
+    model = MoneyManagement
+    form_class = MoneyManagementForm
+    success_url = reverse_lazy('mm_list')
+    success_message = "MM Updated!"
+
+
+class MoneyManagementDelete(SuccessMessageMixin, DeleteView, LoginRequiredMixin):
+    """
+    Delete MoneyManagement
+    """
+    model = MoneyManagement
+    success_url = reverse_lazy('mm_list')
+    success_message = "MM deleted!"
