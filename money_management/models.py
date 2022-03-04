68913ac3918da568d69d2971ec7213daa4755839
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2022-03-04 21:45:50 +0600

    model added

--- money_management/models.py
+++ money_management/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+class Expenses(models.Model):
+    name = models.CharField(max_length=250, blank=True, null=True)
+    amount = models.DecimalField(default=0.0)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Loans(models.Model):
+    name = models.CharField(max_length=250, blank=True, null=True)
+    amount = models.DecimalField(default=0.0)
+    loan_from = models.CharField(max_length=250, blank=True, null=True)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MoneyManagement(models.Model):
+    salary = models.DecimalField(default=0.0)
+    expense = models.ForeignKey(Expenses, on_delete=models.CASCADE, blank=True, null=True)
+    loan = models.ForeignKey(Loans, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.salary
