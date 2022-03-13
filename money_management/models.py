7ae3b88de23acbe4f89003c18efd74c4e1262a50
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2022-03-13 17:15:42 +0600

    update model add images

--- money_management/models.py
+++ money_management/models.py
@@ -1,10 +1,138 @@
 from django.db import models
 
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    instance_name = instance.name.lower()
+    instance_name = instance_name.replace(" ", '-')
+    if instance.check_pdf(filename):
+        instance_name = 'documents/pdf/' + instance_name
+    elif instance.check_doc:
+        instance_name = 'documents/word/' + instance_name
+    else:
+        instance_name = 'user/' + instance_name
+    return '{0}'.format(instance_name)
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=250, blank=True, null=True)
+    document = models.FileField(upload_to=user_directory_path)
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def check_pdf(filename):
+        """
+        Check the file is pdf or not
+        :param filename:
+        :return: Boolean
+        """
+        print('file name', filename)
+        if filename.endswith('.pdf'):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def check_doc(filename):
+        """
+        Check the file is word file or not
+        :param filename:
+        :return: Boolean
+        """
+
+        if filename.endswith('.doc'):
+            return True
+        elif filename.endswith('.docx'):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def check_image(filename):
+        """
+        Check the file is image or not
+        :param filename:
+        :return: Boolean
+        """
+
+        if filename.endswith('.png'):
+            return True
+        elif filename.endswith('.jpg'):
+            return True
+        elif filename.endswith('.jpeg'):
+            return True
+        elif filename.endswith('.gif'):
+            return True
+        elif filename.endswith('.eps'):
+            return True
+        elif filename.endswith('.tif'):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def check_video(filename):
+        """
+        Check the file is video or not
+        :param filename:
+        :return: Boolean
+        """
+        if filename.endswith('.mp4'):
+            return True
+        elif filename.endswith('.mov'):
+            return True
+        elif filename.endswith('.wmv'):
+            return True
+        elif filename.endswith('.avi'):
+            return True
+        elif filename.endswith('.mkv'):
+            return True
+        elif filename.endswith('.mpeg'):
+            return True
+        elif filename.endswith('.flv'):
+            return True
+        elif filename.endswith('.swf'):
+            return True
+        elif filename.endswith('.f4v'):
+            return True
+        elif filename.endswith('.avchd'):
+            return True
+        elif filename.endswith('.webm'):
+            return True
+        elif filename.endswith('.ts'):
+            return True
+
+        else:
+            return False
+
+    def get_file_size(self):
+        """
+        Get Document File size
+        :return: file size
+        """
+        if self.document:
+            return "0 KB"
+        else:
+            value = self.document.size
+            if value < 512000:
+                value = value / 1024.0
+                ext = 'KB'
+            elif value < 4194304000:
+                value = value / 1048576.0
+                ext = 'MB'
+            else:
+                value = value / 1073741824.0
+                ext = 'GB'
+            return '%s %s' % (str(round(value, 2)), ext)
+
+
 class Expenses(models.Model):
     name = models.CharField(max_length=250, blank=True, null=True)
     amount = models.DecimalField(default=0.0, decimal_places=2, max_digits=10)
     date = models.DateField(auto_now=True)
+    additional_files = models.ManyToManyField(Document, blank=True, related_name="expenses")  # Upload multiple files
 
     def __str__(self):
         return self.name
@@ -15,6 +143,7 @@ class Loans(models.Model):
     amount = models.DecimalField(default=0.0, decimal_places=2, max_digits=10)
     loan_from = models.CharField(max_length=250, blank=True, null=True)
     date = models.DateField(auto_now=True)
+    additional_files = models.ManyToManyField(Document, blank=True, related_name="loans")  # Upload multiple files
 
     def __str__(self):
         return self.name
@@ -42,6 +171,8 @@ class MoneyManagement(models.Model):
     year = models.CharField(max_length=10, blank=True, null=True)
     month = models.CharField(max_length=50, choices=CHOICE_MONTH, blank=True, null=True)
     date = models.DateField(auto_now=True)
+    additional_files = models.ManyToManyField(Document, blank=True,
+                                              related_name="money_management")  # Upload multiple files
 
     def __str__(self):
         return str(self.salary) + " In " + self.month + " Month"
